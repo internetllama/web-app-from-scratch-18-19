1b131b6eb4fdfa5e2c08dc29b8ac8b8f9922458a
Melissa Teunisse <melissateunisse@re-congress-113-83.wireless.uva.nl>
2019-02-11 13:50:38 +0100

    New changes

--- week1/public/js/app.js
+++ week1/public/js/app.js
@@ -3,18 +3,16 @@ var request = new XMLHttpRequest();
 request.onload = function () {
 
   var data = JSON.parse(this.response);
-  var storeData = [];
-  console.log(data)
-  element = document.getElementById("list");
-  element.innerHTML = `${data.map((item, i) => `
-  <div class="catBreed">
-    <h1>${item.name}</h1>
-    <img src="./public/img/${item.id}.jpg" style="display:block;width:100%;max-width:600px">
-    <p>${item.origin}</p>
-    <p>${item.temperament}</p>
-  </div>
-  `).join("")}`;
+  var element = document.getElementById("list");
+  element.innerHTML = data.map((item, i) => `
+    <div class="catBreed">
+      <h1>${item.name}</h1>
+      <img src="./public/img/${item.id}.jpg" class="imgCat">
+      <p>${item.origin} <img src="./public/img/${item.origin}.png" class="imgFlag"></p>
+      <p>${item.temperament}</p>
+    </div>
+    `).join("");
 }
 
-request.open('GET', 'https://api.thecatapi.com/v1/breeds', true);
+request.open("GET", "https://api.thecatapi.com/v1/breeds", "true");
 request.send();
